085e2c2c7a2d3941fec66a3569f5f36fc0dff057
ahmedessam <ahmed.essam.developer.flutter@gmail.com>
2026-06-05 03:29:41 +0300

    add icon and splash screen to the app.

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,5 @@
+abstract class AppAssets {
+  static const String basePath = 'assets/images/';
+  static const String fullLogo = '${basePath}full_logo.png';
+  static const String splash = '${basePath}splash.png';
+}
